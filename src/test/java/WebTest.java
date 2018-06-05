2e49219c4e8ad52b5765ee7a9e12565c25ff06d7
Jelena <Jelenna>
2018-06-05 19:58:03 +0300

    Initial commit

---
+++ src/test/java/WebTest.java
@@ -0,0 +1,31 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebTest {
+
+    private final String DELFI_PAGE = "http://rus.delfi.lv/";
+    private final By NEWS_NAME = By.xpath(".//a[@class='top2012-title']");
+
+
+    @Test
+
+    public void pageTest() {
+
+        System.setProperty("webdriver.gecko.driver", "C:/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get(DELFI_PAGE);
+
+        WebElement newsName = driver.findElement(NEWS_NAME);
+        newsName.click();
+        //newsName.sendKeys("Test");
+        //Assert.assertEquals("", "");
+        driver.quit();
+
+    }
+
+}
