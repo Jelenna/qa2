e1a15632aa54a52172c2ba2a17fedf8f16f14b32
Jelena <Jelenna>
2018-06-10 18:10:35 +0300

    edit

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WebTest {
@@ -22,10 +23,29 @@ public class WebTest {
 
         WebElement newsName = driver.findElement(NEWS_NAME);
         newsName.click();
+
         //newsName.sendKeys("Test");
-        //Assert.assertEquals("", "");
+        Assert.assertEquals("Во время учений на полигоне в Адажи произошел пожар: загорелся лес - DELFI", driver.getTitle());
         driver.quit();
 
     }
+
+    @Test
+
+    public void pageTest2() {
+
+        System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(DELFI_PAGE);
+
+        WebElement newsName = driver.findElement(NEWS_NAME);
+        newsName.click();
+        //newsName.sendKeys("Test");
+        Assert.assertEquals("Во время учений на полигоне в Адажи произошел пожар: загорелся лес - DELFI", driver.getTitle());
+
+        driver.quit();
+    }
+
 
 }
